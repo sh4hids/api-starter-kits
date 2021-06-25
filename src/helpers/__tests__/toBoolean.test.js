b884fb43fdbafcf0be24fcd85b719f02bf08634a
sh4hids <dev@shahid.pro>
2021-06-25 14:29:26 +0600

    update test case

--- src/helpers/__tests__/toBoolean.test.js
+++ src/helpers/__tests__/toBoolean.test.js
@@ -1,9 +1,19 @@
-import toBoolean from "../toBoolean";
+import toBoolean from '../toBoolean';
 
-describe("# toBoolean", () => {
-  const truthyInputs = ["true", true, 1, "1", "yes", "Yes", "Y", "y"];
-  const falsyInputs = ["false", false, 0, "0", "no", "No", "N", "n"];
-  const nullInputs = [null, "", "sdfas"];
+describe('# toBoolean', () => {
+  const truthyInputs = ['true', true, 1, '1', 'yes', 'Yes', 'Y', 'y'];
+  const falsyInputs = [
+    'false',
+    false,
+    0,
+    '0',
+    'no',
+    'No',
+    'N',
+    'n',
+    null,
+    undefined,
+  ];
 
   truthyInputs.forEach((value) => {
     it(`should convert ${value} to true`, () => {
@@ -16,13 +26,6 @@ describe("# toBoolean", () => {
     it(`should convert ${value} to false`, () => {
       const result = toBoolean(value);
       expect(result).toBe(false);
-    });
-  });
-
-  nullInputs.forEach((value) => {
-    it(`should convert "${value}" to "${value}"`, () => {
-      const result = toBoolean(value);
-      expect(result).toBe(value);
     });
   });
 });
